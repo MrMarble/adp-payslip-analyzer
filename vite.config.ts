5c2bb76f9add4d44951eb8fe6e7d7a17f0f6a719
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2026-02-06 17:25:34 +0100

    Set base path for Vite configuration to support deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/adp-payslip-analyzer/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
